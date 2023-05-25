d19651cdc97bd3c56f1dc0d05ffc634751439c0b
JoseTrujilloSilva <96332768+JoseTrujilloSilva@users.noreply.github.com>
2023-05-25 17:28:19 +0200

    translates confi

--- index/pagina/app/conf/User/confUser.js
+++ index/pagina/app/conf/User/confUser.js
@@ -1,9 +1,9 @@
 $(window).on('load', codigo);
 
 function codigo() {
-
+    indiceIdiomas();
     idUser = window.location.href.split('?')[1].split('=')[1];
-    
+    $('#eligeIdioma').on('change', eventoIdioma);
     $('#menuPerfil').on('click', eventoPerfil);
     $('#menuConfig').on('click', eventoConfig);
     $('#menuUser').on('click', eventoUser);
@@ -168,13 +168,11 @@ function codigo() {
         let contCHeck = 0;
 
         for (const value of data) {
-            document.getElementById('formDesbUsuarios').innerHTML += '<div class="row p-4 align-items-center"><div class="col-auto"><input type="hidden" name="idUser" id="idUser" value="'+idUser+'"><input type="checkbox" value="'+value[2]+'" name="bloq[]" id="bloq'+contCHeck+++'"></div><div class="col-3 col-sm-auto my-3 my-sm-0"><img class="rounded-circle" width="50" height="50" src="../../../'+value[1]+'" alt=""></div><div class="col-5 col-sm-7"><span>'+value[0]+'</span></div><div class="col-12 col-sm-auto text-center text-sm-end mx-0 mx-lg-5 my-2 my-sm-0"><span>Bloqueado: </span><span id="span'+value[2]+'"></span></div></div>';
+            document.getElementById('formDesbUsuarios').innerHTML += '<div class="col-auto"><input type="hidden" name="idUser" id="idUser" value="'+idUser+'"><input type="checkbox" value="'+value[2]+'" name="bloq[]" id="bloq'+contCHeck+++'"></div><div class="col-3 col-sm-auto my-3 my-sm-0"><img class="rounded-circle" width="50" height="50" src="../../../'+value[1]+'" alt=""></div><div class="col-5 col-sm-7"><span>'+value[0]+'</span></div><div class="col-12 col-sm-auto text-center text-sm-end mx-0 mx-lg-5 my-2 my-sm-0"><span>Bloqueado: </span><span id="span'+value[2]+'"></span></div>';
 
 
             document.getElementById('span'+value[2]+'').innerHTML = 'No';
         }
-        
-        document.getElementById('formDesbUsuarios').innerHTML += '<button class="btn btn-danger my-3 w-100" type="submit">Bloquear/Desbloquear</button>';
 
         fetch('../UsuariosBack/bloq/devuelveBloq.php', {
             url: '../UsuariosBack/bloq/devuelveBloq.php',
@@ -202,34 +200,14 @@ function codigo() {
 
     })
 
-    let dataUser = new FormData();
 
-    dataUser.append('idUser', idUser);
+
+
 
     fetch('../ConfigPerfil/tarians/tarians.php', {
         url: '../ConfigPerfil/tarians/tarians.php',
         method: 'POST',
-        body: dataUser
-    })
-    .then(function(res){
-        return res.json();
-    })
-    .then(function(data){
-        let contCHeck = 0;
-
-        for (const value of data) {
-            document.getElementById('deleteUserTariansForm').innerHTML += '<div class="row p-4 align-items-center"><div class="col-auto"><input type="hidden" name="idUserTariansUser" id="idUserTariansUser" value="'+idUser+'"></div><div class="col-12 col-sm-7"><div class="row"><div class="col-10 col-sm-auto my-3 my-sm-0"><input type="checkbox" value="'+value[6]+'" name="tariansUser[]" id="tariansDelete2'+contCHeck+++'"><span style="padding-left: 10px;">'+value[5]+'</span></div><div class="col-10 col-sm-7"><span>'+value[0]+'</span></div></div></div><div class="col-12 col-sm-4"><span>Fecha: </span><span>'+value[4]+'</span></div></div>';
-        }
-        
-        document.getElementById('deleteUserTariansForm').innerHTML += '<div clas="row"><div class="col px-3"><button class="btn btn-danger my-3 w-100" type="submit">Eliminar Tarians</button><div></div>';
-    })
-
-
-
-    fetch('../ConfigPerfil/comentarios/comments.php', {
-        url: '../ConfigPerfil/comentarios/comments.php',
-        method: 'POST',
-        body: dataUser
+        body: dataAdmin
     })
     .then(function(res){
         return res.json();
@@ -239,10 +217,27 @@ function codigo() {
         console.log(data);
 
         for (const value of data) {
-            document.getElementById('deleteUserCommentsForm').innerHTML += '<div class="row p-4 align-items-center"><div class="col-auto"><input type="hidden" name="idUserComments" id="idUserComments" value="'+idUser+'"></div><div class="col-12 col-sm-7"><div class="row"><div class="col-10 col-sm-auto my-3 my-sm-0"><input type="checkbox" value="'+value[3]+'" name="comments[]" id="comentarioDelete'+contCHeck+++'"><span style="padding-left: 10px;">'+value[2]+'</span></div><div class="col-10 col-sm-7"><span>'+value[0]+'</span></div></div></div><div class="col-12 col-sm-4"><span>Fecha: </span><span>'+value[1]+'</span></div></div>';
+            document.getElementById('deleteUserTariansForm').innerHTML += '<div class="col-auto"><input type="hidden" name="idUserTariansUser" id="idUserTarians" value="'+idUser+'"></div><div class="col-12 col-sm-7"><div class="row"><div class="col-10 col-sm-auto my-3 my-sm-0"><input type="checkbox" value="'+value[6]+'" name="tariansUser[]" id="tariansDelete'+contCHeck+++'"><span style="padding-left: 10px;">'+value[5]+'</span></div><div class="col-10 col-sm-7"><span>'+value[0]+'</span></div></div></div><div class="col-12 col-sm-4"><span>Fecha: </span><span>'+value[4]+'</span></div>';
         }
-        
-        document.getElementById('deleteUserCommentsForm').innerHTML += '<div clas="row"><div class="col px-3"><button class="btn btn-danger my-3 w-100" type="submit">Eliminar Comentarios</button><div></div>';
+    })
+
+
+
+    fetch('../ConfigPerfil/comentarios/comments.php', {
+        url: '../ConfigPerfil/comentarios/comments.php',
+        method: 'POST',
+        body: dataAdmin
+    })
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        let contCHeck = 0;
+        console.log(data);
+
+        for (const value of data) {
+            document.getElementById('deleteUserCommentsForm').innerHTML += '<div class="col-auto"><input type="hidden" name="idUserComments" id="idUserComments" value="'+idUser+'"></div><div class="col-12 col-sm-7"><div class="row"><div class="col-10 col-sm-auto my-3 my-sm-0"><input type="checkbox" value="'+value[3]+'" name="comments[]" id="comentarioDelete'+contCHeck+++'"><span style="padding-left: 10px;">'+value[2]+'</span></div><div class="col-10 col-sm-7"><span>'+value[0]+'</span></div></div></div><div class="col-12 col-sm-4"><span>Fecha: </span><span>'+value[1]+'</span></div>';
+        }
     })
 
 
@@ -273,6 +268,86 @@ function codigo() {
         
     }
 
+    function indiceIdiomas() {
+        fetch('../Admin/confIdiomaAdmin.json')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        console.log(data)
+        var idioma = localStorage.getItem('localLanguajes');
+        if (idioma===undefined || idioma === 'null') {
+            idioma = sessionStorage.getItem('sesionLanguajes');
+        }
+            //START PAGE
+            $('#txtHome').html(data[idioma].home);
+            $('#atras').html(data[idioma].back);
+            //PERFIL
+            $('#perfil23').html(data[idioma].profile);
+            $('#textAdministrador').html(data[idioma].interiorProfile.admin);
+            $('#textUsuariosAdmin').html(data[idioma].interiorProfile.interiorAdmin.users);
+            $('#textComentariosAdmin').html(data[idioma].interiorProfile.comments);
+            $('#textComunidadAdmin').html(data[idioma].interiorProfile.community);
+            for (const value of  document.getElementsByClassName('botonDelete')) {
+                value.innerHTML = data[idioma].interiorProfile.interiorAdmin.interiorUser.deleteButton;
+            }
+            $('#textInfo01').html(data[idioma].interiorProfile.interiorCommunity.keyss.info);
+            $('.keyssAdmin').html(data[idioma].interiorProfile.interiorCommunity.keyss.keyss);
+            $('.passwordAdmin').html(data[idioma].interiorProfile.interiorCommunity.keyss.password);
+            $('#keyssComAdmin').html(data[idioma].interiorProfile.interiorCommunity.deleteCom.communityKeyss);
+            $('#passwordComAdmin').html(data[idioma].interiorProfile.interiorCommunity.deleteCom.communityPassword);
+            $('.keyssAdminComUser').html(data[idioma].interiorProfile.interiorCommunity.deleteCom.userKeyss);
+            $('.passwordAdminComUser').html(data[idioma].interiorProfile.interiorCommunity.deleteCom.userPassword);
+            $('.passwordRepeat').html(data[idioma].interiorProfile.interiorCommunity.password.repeatPassword);
+            $('.passwordNewCom').html(data[idioma].interiorProfile.interiorCommunity.password.newPassword);
+            $('#nuevaImagen').html(data[idioma].interiorProfile.interiorCommunity.img.img);
+            $('#newNameCom').html(data[idioma].interiorProfile.interiorCommunity.name.name);
+            $('#textInfo02').html(data[idioma].interiorProfile.interiorCommunity.password.info);
+            $('#textInfo03').html(data[idioma].interiorProfile.interiorCommunity.img.info);
+            $('.send').html(data[idioma].interiorProfile.interiorCommunity.img.button);
+            $('#textInfo04').html(data[idioma].interiorProfile.interiorCommunity.name.info);
+            $('#textInfo05').html(data[idioma].interiorProfile.interiorCommunity.deleteCom.info);
+            $('.botonDelete').html(data[idioma].interiorProfile.interiorAdmin.interiorUser.deleteButton);
+            $('#editNamePerfil').html(data[idioma].editName);
+            $('.nameUserText').html(data[idioma].interiorEditName.name);
+            $('.passwordUserText').html(data[idioma].interiorEditName.password);
+            $('#cambiaImgtext').html(data[idioma].interiorEditImg.img);
+            $('#deletePerfilText').html(data[idioma].deleteProfile);
+            //END PERFIL
+            //START CONFIGURACION
+            $('#chooseLanguage').html(data[idioma].interiorConfig.interior.language);
+            $('#option0').html(data[idioma].interiorConfig.interior.language);
+            $('#perm').html(data[idioma].interiorConfig.interior.button);
+            $('.comments').html(data[idioma].interiorConfig.comments);
+            //END CONFIGURACION
+            //START USERS
+            $('.bloqDesbloq').html(data[idioma].interiorUser.interior.button);
+            //END USERS
+            //START ACCOUNT
+            $('.passwordAccount').html(data[idioma].interiorAccount.password);
+            $('#infopasswordAccount').html(data[idioma].interiorAccount.interiorPassword.info);
+            $('#keyssAccount').html(data[idioma].interiorAccount.keyss);
+            $('#keyssAccountInfo').html(data[idioma].interiorAccount.interiorKeyss.info);
+            //END ACCOUNT
+            $('#configuracion').html(data[idioma].config);
+            $('#usuariostext').html(data[idioma].users);
+            $('#cuentatext').html(data[idioma].account);
+            $('#cerrarSession').html(data[idioma].logout);
+            //END PAGE
+    })
+    }
     
+
+    function eventoIdioma() {
+        if (document.getElementById('permanente').checked) {
+            localStorage.setItem('localLanguajes', this.value);
+            languaje = localStorage.getItem('localLanguajes');
+        }else{
+            sessionStorage.setItem('sesionLanguajes', this.value);
+            languaje = sessionStorage.getItem('sesionLanguajes');
+            localStorage.setItem('localLanguajes', null);
+        }
+        indiceIdiomas();
+    }
 
 }
